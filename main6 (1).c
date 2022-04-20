bb0e7ac3e8e25c3c028f167d44d9c57dce29a7fe
Fernanda Nolasco <104088221+paolafdanol@users.noreply.github.com>
2022-04-20 12:39:21 -0500

    Add files via upload

---
+++ main6 (1).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+int a;
+printf("Escriba un caracter.\n");
+scanf("%c",&a);
+printf("el resultado es:%c",a);
+}
